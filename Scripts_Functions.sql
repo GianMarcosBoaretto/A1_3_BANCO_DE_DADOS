52d8c0457d5169855e74feefc0fd3ef691acd6e8
GianMarcosBoaretto <130662080+GianMarcosBoaretto@users.noreply.github.com>
2025-06-16 21:27:33 -0300

    Update Scripts_Functions.sql

--- Scripts_Functions.sql
+++ Scripts_Functions.sql
@@ -1,29 +1,34 @@
 DELIMITER $$
 
-CREATE FUNCTION nome_grupo(p_id INT)
-RETURNS VARCHAR(100)
-DETERMINISTIC
-BEGIN
-    DECLARE v_nome VARCHAR(100);
-
-    SELECT nome INTO v_nome FROM grupos_estudo WHERE id = p_id;
-
-    RETURN IFNULL(v_nome, 'Grupo não encontrado');
-END$$
-
-DELIMITER ;
-
-DELIMITER $$
-
 CREATE FUNCTION total_grupos_usuario(p_usuario_id INT)
 RETURNS INT
 DETERMINISTIC
 BEGIN
     DECLARE v_total INT;
 
-    SELECT COUNT(*) INTO v_total FROM usuarios_grupos WHERE usuario_id = p_usuario_id;
+    SELECT COUNT(*) INTO v_total
+    FROM usuario_grupo
+    WHERE id_usuario = p_usuario_id;
 
     RETURN v_total;
 END$$
 
-DELIMITER ;
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE FUNCTION nome_curso_grupo(p_grupo_id INT)
+RETURNS VARCHAR(255)
+DETERMINISTIC
+BEGIN
+    DECLARE v_nome_curso VARCHAR(255);
+
+    SELECT c.descricao INTO v_nome_curso
+    FROM grupos_estudo g
+    JOIN cursos c ON g.id_curso = c.id
+    WHERE g.id = p_grupo_id;
+
+    RETURN IFNULL(v_nome_curso, 'Curso não encontrado');
+END$$
+
+DELIMITER ;
